3a11236f7a61d3a5bc21676473ffa124537cce5d
alanfarias97 <alanfarias_97@hotmail.com>
2026-06-11 18:44:58 -0300

    initial commit

---
+++ src/main/java/com/turnerochubut/api/TurneroChubutApiApplication.java
@@ -0,0 +1,13 @@
+package com.turnerochubut.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurneroChubutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurneroChubutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnerochubut/api/TurneroChubutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnerochubut.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurneroChubutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
